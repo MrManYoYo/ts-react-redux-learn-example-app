b24eba1df2ac11b649456381103b340e0e7ea0d3
MrManYoYo <sky_variant@126.com>
2022-07-27 10:32:52 +0800

    update

---
+++ src/features/navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <section>
+        <h1>Redux Essentials Example</h1>
+
+        <div className="navContent">
+          <div className="navLinks">
+          </div>
+        </div>
+      </section>
+    </nav>
+  );
+}
+
+export default Navbar;
